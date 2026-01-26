58f6eccfb02b02d4a8ffeb90be8b9160855f4914
luacoder-byte <hubtbao01234@gmail.com>
2026-01-26 12:42:24 +0700

    Create Loadersredzcampuchiav5.lua

---
+++ Loadersredzcampuchiav5.lua
@@ -0,0 +1,10 @@
+-- trash 1108080471268663486 lol bot xhider Experts obtain source code by obfuscating it, then send the source code back to their owner. You should not use it.
+-- rác 1108080471268663486 lol bot xhider Các chuyên gia lấy mã nguồn bằng cách làm xáo trộn nó, sau đó gửi mã nguồn lại cho chủ sở hữu. Bạn không nên sử dụng nó.
+spawn(function()
+        loadstring(game:HttpGet(
+            "https://api.luarmor.net/files/v4/loaders/066cfd8aed9b5196e7abb1c17ca424f5.lua"
+        ))()
+    end)
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/luacoder-byte/luacoder/refs/heads/main/Redzhubcampuchiav5.lua"))()
